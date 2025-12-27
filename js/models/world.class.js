234c2e99709acf6c32c45d7701dce20e7d64b9e7
iamDrini <valdrin.murselji@outlook.de>
2025-12-27 15:18:36 +0100

    added background class and modified draw function

--- js/models/world.class.js
+++ js/models/world.class.js
@@ -9,6 +9,10 @@ class World{
         new Cloud()
     ];
 
+    backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png')
+    ]
+
     canvas;
     ctx;
 
@@ -22,13 +26,17 @@ class World{
         //Clear Canvas
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+        this.addToMap(this.character);
         
         this.enemies.forEach(enemy => {
-            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+            this.addToMap(enemy);
         })
         this.clouds.forEach(cloud => {
-            this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
+            this.addToMap(cloud);
+        })
+
+        this.backgroundObjects.forEach(background => {
+            this.addToMap(background)
         })
         
 
@@ -37,5 +45,9 @@ class World{
         requestAnimationFrame(function(){
             self.draw();
         });
+    }
+
+    addToMap(mo){
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
     }
 }
