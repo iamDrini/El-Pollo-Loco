8b8348e583bab51040e293be3b7baa24949c20c5
iamDrini <valdrin.murselji@outlook.de>
2025-12-28 22:33:24 +0100

    added isColliding function

--- js/models/movable-object.class.js
+++ js/models/movable-object.class.js
@@ -42,6 +42,13 @@ class MovableObject {
             ctx.stroke();
         }
     }
+    //character.isColliding(chicken)
+    isColliding(mo){
+        return this.x + this.width > mo.x && 
+            this.y + this.height > mo.y &&
+            this.x  < mo.x &&
+            this.y < mo.y + mo.height;
+    }
     loadImages(arr) {
         arr.forEach((path) => {
             let img = new Image();
--- js/models/world.class.js
+++ js/models/world.class.js
@@ -17,21 +17,33 @@ class World {
         this.keyboard = keyboard;
         this.draw();
         this.setWorld();
+        this.checkCollisions();
     }
 
     setWorld() {
-    this.character.world = this;
-    this.level.coins.forEach(coin => {
-        coin.world = this;
-        coin.initPosition();
-    });
-}
+        this.character.world = this;
+        this.level.coins.forEach(coin => {
+            coin.world = this;
+            coin.initPosition();
+        });
+
+    }
+
+    checkCollisions(){
+        setInterval(() => {
+            this.level.enemies.forEach((enemy) => {
+                if(this.character.isColliding(enemy)){
+                    console.log('Collision with character');
+                }
+            });
+        },200);
+    }
 
     draw() {
         //Clear Canvas
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        this.ctx.translate(this.camera_x,0);
+        this.ctx.translate(this.camera_x, 0);
 
         this.addObjectsToMap(this.level.backgroundObjects);
         this.addObjectsToMap(this.level.enemies);
@@ -39,7 +51,7 @@ class World {
         this.addToMap(this.character);
         this.addObjectsToMap(this.level.clouds);
 
-        this.ctx.translate(-this.camera_x,0);
+        this.ctx.translate(-this.camera_x, 0);
 
 
         //Draw() wird in Schleife aufgerufen
@@ -50,24 +62,24 @@ class World {
     }
 
     addToMap(mo) {
-        if(mo.otherDirection){
+        if (mo.otherDirection) {
             this.flipImage(mo);
         }
         mo.draw(this.ctx);
         mo.drawFrame(this.ctx);
 
-        if(mo.otherDirection){
+        if (mo.otherDirection) {
             this.flipImageBack(mo);
         }
     }
-    flipImage(mo){
+    flipImage(mo) {
         this.ctx.save();
-            this.ctx.translate(mo.width, 0);
-            this.ctx.scale(-1, 1);
-            mo.x = mo.x * -1;
+        this.ctx.translate(mo.width, 0);
+        this.ctx.scale(-1, 1);
+        mo.x = mo.x * -1;
     }
 
-    flipImageBack(mo){
+    flipImageBack(mo) {
         mo.x = mo.x * -1;
         this.ctx.restore();
     }
