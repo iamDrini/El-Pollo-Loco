99dde3561a653a46e7a08bf14ca7e57802e999ca
iamDrini <valdrin.murselji@outlook.de>
2025-12-28 03:16:10 +0100

    endboss added

--- js/models/movable-object.class.js
+++ js/models/movable-object.class.js
@@ -1,34 +1,42 @@
-class MovableObject{
+class MovableObject {
     x = 120;
     y = 280;
     img;
     height = 150;
     width = 100;
     speed = 0.5;
-    imageCache={};
+    imageCache = {};
     currentImage = 0;
     otherDirection = false;
 
-    loadImage(path){
+    loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
 
-    loadImages(arr){
+    loadImages(arr) {
         arr.forEach((path) => {
             let img = new Image();
             img.src = path;
             this.imageCache[path] = img;
         });
     }
-    moveLeft(){
-        setInterval(() => {
-            this.x -= this.speed;
-        }, 1000/60);
+
+    playAnimation(images) {
+        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let path = images[i];
+        this.img = this.imageCache[path]
+        this.currentImage++;
     }
 
-    moveRight(){
+    moveLeft() {
+        setInterval(() => {
+            this.x -= this.speed;
+        }, 1000 / 60);
+    }
+
+    moveRight() {
         console.log('moving right');
-        
+
     }
 }
