5c157419425104558f642da964c956ac77ed04ab
iamDrini <valdrin.murselji@outlook.de>
2025-12-30 12:24:35 +0100

    Bottle splash animation

--- js/models/throwable-object.class.js
+++ js/models/throwable-object.class.js
@@ -1,21 +1,63 @@
-class ThrowableObject extends CollactableObject{
+class ThrowableObject extends CollactableObject {
+    isAboveGround() {
+        return !this.isSplashed;
+    }
 
-    constructor(x,y){
+    IMAGE_SPLASH = [
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png'
+    ]
+
+    isSplashed = false;
+
+    constructor(x, y) {
         super().loadImage('img/6_salsa_bottle/salsa_bottle.png');
-        this.x = x+50;
-        this.y = y+100;
+        this.loadImages(this.IMAGE_SPLASH);
+        this.x = x + 50;
+        this.y = y + 100;
         this.height = 70;
         this.width = 70;
         this.throw();
+        this.animate();
     }
 
-    throw(){
+    throw() {
         this.speedY = 13;
         this.applyGravity();
-        setInterval(() => {
-            this.x += 15;
+        this.throwInterval = setInterval(() => {
+            if (!this.isSplashed) {
+                this.x += 15;
+            }
         }, 25);
     }
+
+    animate() {
+        const groundLevel = 450;
+        this.animateInterval = setInterval(() => {
+            if (!this.isSplashed && this.y + this.height >= groundLevel) {
+                this.y = groundLevel - this.height;
+                this.isSplashed = true;
+                this.speedY = 0;
+                this.startSplashAnimation();
+            }
+        }, 50);
+    }
+
+    startSplashAnimation() {
+        this.currentImage = 0;
+        let splashIndex = 0;
+        this.splashInterval = setInterval(() => {
+            if (splashIndex < this.IMAGE_SPLASH.length) {
+                let path = this.IMAGE_SPLASH[splashIndex];
+                this.img = this.imageCache[path];
+                splashIndex++;
+            } else {
+                clearInterval(this.splashInterval);
+            }
+        }, 60);
+    }
 }
-//nur für die Flaschen zum Werfen!
-//Für einsammeln und zeigen Klasse bottles verwenden
