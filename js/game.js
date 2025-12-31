e2f38b72c4e5a9f28afa45d46797cc76cdeaf77f
iamDrini <valdrin.murselji@outlook.de>
2025-12-31 01:59:54 +0100

    added fullscreen mode

--- js/game.js
+++ js/game.js
@@ -51,3 +51,26 @@ window.addEventListener('keyup', (e) => {
     }
 
 });
+
+function fullscreen(){
+    let fullscreen = document.getElementById('canvas');
+    enterFullscreen(fullscreen);
+}
+
+function enterFullscreen(element) {
+  if(element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if(element.msRequestFullscreen) {   
+    element.msRequestFullscreen();
+  } else if(element.webkitRequestFullscreen) {  
+    element.webkitRequestFullscreen();
+  }
+}
+
+function exitFullscreen() {
+  if(document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if(document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
